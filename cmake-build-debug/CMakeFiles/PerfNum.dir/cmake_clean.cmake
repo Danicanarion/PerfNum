4eac97bcaa95fb08602ffb5f597b3c9aeb9dc62d
Daniel Delgado Perera <Dadepe@localhost.localdomain>
2018-11-10 14:10:06 +0000

    Primer commit, estructura básica con el submódulo yieldxx

---
+++ cmake-build-debug/CMakeFiles/PerfNum.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PerfNum.dir/main.cpp.o"
+  "PerfNum.pdb"
+  "PerfNum"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PerfNum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
